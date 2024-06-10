c518d3707e3827ad7079d5644c27e6f8abd60af1
hassanbabar78 <130081518+hassanbabar78@users.noreply.github.com>
2024-06-10 15:10:56 +0500

    posts functions handle

---
+++ components/posts/CommentItem.tsx
@@ -0,0 +1,97 @@
+import { useRouter } from 'next/router';
+import { useCallback, useMemo } from 'react';
+import { formatDistanceToNowStrict } from 'date-fns';
+
+import Avatar from '../Avatar';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { FaTrash } from 'react-icons/fa';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+
+interface CommentItemProps {
+  data: Record<string, any>;
+}
+
+const CommentItem: React.FC<CommentItemProps> = ({ data = {} }) => {
+  const router = useRouter();
+  const currentuser = useCurrentUser();
+
+  const goToUser = useCallback((ev: any) => {
+    ev.stopPropagation();
+
+    router.push(`/users/${data.user.id}`)
+  }, [router, data.user.id]);
+
+  const createdAt = useMemo(() => {
+    if (!data?.createdAt) {
+      return null;
+    }
+
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data.createdAt])
+  const handleDelete = async (postId: string) => {  
+    try {
+      await axios.delete(`/api/deleteComment?id=${postId}`);
+      console.log(data)
+      toast.success('Post deleted successfully');
+      router.push('/'); // Redirect to the home page or any other page
+    } catch (error) {
+      console.log('Error deleting post:', error);
+      toast.error('Failed to delete post');
+    }
+  };
+
+  return (
+    <div 
+      className="
+        border-b-[1px] 
+        border-neutral-800 
+        p-5 
+        cursor-pointer 
+        hover:bg-neutral-900 
+        transition
+      ">
+      <div className="flex flex-row items-start gap-3">
+        <Avatar userId={data.user.id} />
+        <div>
+          <div className="flex flex-row items-center gap-2">
+            <p 
+              onClick={goToUser} 
+              className="
+                text-white 
+                font-semibold 
+                cursor-pointer 
+                hover:underline
+            ">
+              {data.user.name}
+            </p>
+            <span 
+              onClick={goToUser} 
+              className="
+                text-neutral-500
+                cursor-pointer
+                hover:underline
+                hidden
+                md:block
+            ">
+              @{data.user.username}
+            </span>
+            <span className="text-neutral-500 text-sm">
+              {createdAt}
+            </span>
+            {currentuser && currentuser.data.id === data.user.id && ( // Use currentUserData instead of currentUser
+              <button onClick={() => handleDelete(data.id)} className="text-neutral-500 hover:text-red-500">
+                <FaTrash />
+              </button>
+            )}
+          </div>
+          <div className="text-white mt-1">
+            {data.body}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CommentItem;
